8a626b041928a08a5cad76cab39dbf6341811fb7
Almin Iriskic <almin.iriskic@easelink.com>
2022-05-17 01:11:46 +0200

    integrate esp32-toolchain from flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,26 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    esp32-toolchain.url = "github:almini/nix-esp32-toolchain";
+    esp32-toolchain.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils, esp32-toolchain }: {
     overlay = import ./overlay.nix;
   } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+      pkgs = import nixpkgs { 
+        inherit system; 
+        overlays = [ 
+          self.overlay 
+          (final: prev: {
+            gcc-xtensa-esp32-elf-bin = esp32-toolchain.defaultPackage.${system};
+          })
+        ]; 
+      };
     in
     {
-      packages = {
+      packages = builtins.trace esp32-toolchain.defaultPackage.${system} {
         inherit (pkgs)
           gcc-xtensa-esp32-elf-bin
           openocd-esp32-bin
--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,6 @@
 final: prev: 
 {
-  gcc-xtensa-esp32-elf-bin = prev.callPackage ./pkgs/esp32-toolchain-bin.nix { };
+  # gcc-xtensa-esp32-elf-bin = prev.callPackage ./pkgs/esp32-toolchain-bin.nix { };
   openocd-esp32-bin = prev.callPackage ./pkgs/openocd-esp32-bin.nix { };
   esp-idf-src = prev.callPackage ./pkgs/esp-idf-src { };
 }
